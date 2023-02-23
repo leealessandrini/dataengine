fc98c17372b37144464e50da025e2389ba3cb36c
Lee Alessandrini <Daniel_Alessandrini@comcast.com>
2023-02-23 15:29:14 -0500

    v0.0.12: Added collect_query_result and check_table_existence methods to query driver.

--- capsulecorp/query_driver.py
+++ capsulecorp/query_driver.py
@@ -2,6 +2,31 @@
 import logging
 
 
+def connect_to_db(driver, driver_args):
+    """
+        This method will establish a connection to the Redshift database.
+
+        Args:
+            driver (): database driver module
+            driver_args (dict): map of database connection args
+
+        Returns:
+            db connnection, connection cursor, and success bool
+    """
+    connection_success = True
+    try:
+        conn = driver.connect(**driver_args)
+        cur = conn.cursor()
+        logging.info("Database connection successful.")
+    except Exception:
+        conn = None
+        cur = None
+        connection_success = False
+        logging.error("Database connection failure.", exc_info=True)
+
+    return conn, cur, connection_success
+
+
 def execute_query(conn, cur, query, message=None):
     """
         This method will safely execute the given query and log the result.
@@ -31,26 +56,50 @@ def execute_query(conn, cur, query, message=None):
     return success
 
 
-def connect_to_db(driver, driver_args):
+def collect_query_result(conn, query, message):
     """
-        This method will establish a connection to the Redshift database.
+        This method will safely collect the result of a query and return it as
+        a pandas DataFrame.
 
         Args:
-            driver (): database driver module
-            driver_args (dict): map of database connection args
+            conn (psycopg2.extensions.connection): db connection
+            query (str): sql query string
+            message (str): logging message corresponging to query
 
         Returns:
-            db connnection, connection cursor, and success bool
+            query result and success boolean
     """
-    connection_success = True
+    success = True
     try:
-        conn = driver.connect(**driver_args)
-        cur = conn.cursor()
-        logging.info("Database connection successful.")
+        result = pd.read_sql(query, con=conn)
+        logging.info(message + " success.")
     except Exception:
-        conn = None
-        cur = None
-        connection_success = False
-        logging.error("Database connection failure.", exc_info=True)
+        success = False
+        result = pd.DataFrame()
+        logging.error(message + " failed.", exc_info=True)
 
-    return conn, cur, connection_success
+    return result, success
+
+
+def check_table_existence(conn, table_name):
+    """
+        This method will check the database schema for the provided table.
+
+        Args:
+            conn (pymysql.connections.Connection): database connection
+            table_name (str): database table name
+
+        Returns:
+            existence boolean
+    """
+    table_exists = False
+    # Check whether date table exists
+    stmt = f"SHOW TABLES LIKE '{table_name}'"
+    cur = conn.cursor()
+    cur.execute(stmt)
+    result = cur.fetchone()
+    if result:
+        table_exists = True
+    cur.close()
+
+    return table_exists
