fac3d9f8090f5c54bf6a463d5ef67097366788d7
Lee Alessandrini <38591905+leealessandrini@users.noreply.github.com>
2023-05-15 15:58:16 -0400

    v0.0.21: Updated slackbot module to include method for posting dataframes as CSV files.

--- capsulecorp/slackbot.py
+++ capsulecorp/slackbot.py
@@ -1,11 +1,12 @@
 """
 This module will house all automated slack app logic.
 """
+import io
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 
-def post_pandas_dataframe(channel, token, pandas_df, message):
+def post_dataframe_as_message(channel, token, pandas_df, message):
     """
         Posts a pandas DataFrame to a Slack channel.
 
@@ -31,3 +32,32 @@ def post_pandas_dataframe(channel, token, pandas_df, message):
         print("Message posted to Slack")
     except SlackApiError as e:
         print("Error posting message to Slack: {}".format(e))
+
+
+def post_dataframe_as_csv(channel_id, token, pandas_df, filename):
+    """
+        Posts a pandas DataFrame to a Slack channel.
+
+        Args:
+            channel_id (str): The ID or name of the Slack channel to post to.
+            token (str): The Slack API token to use for authentication.
+            pandas_df (pd.DataFrame): The DataFrame to post to the channel.
+            filename (str): Filename for downloadable CSV.
+
+        Returns:
+            None.
+    """
+    # Write the DataFrame to a CSV in a buffer
+    csv_buffer = io.StringIO()
+    pandas_df.to_csv(csv_buffer, index=False)
+    # Initialize a Slack WebClient instance
+    client = WebClient(token=token)
+    # Upload the CSV to a Slack channel
+    try:
+        response = client.files_upload_v2(
+            channel=channel_id, filename=filename,
+            content=csv_buffer.getvalue(),
+            initial_comment="*~~~ CSV DOWNLOAD ~~~*")
+        print("CSV posted to Slack")
+    except SlackApiError as e:
+        print("Error posting CSV to Slack: {}".format(e))
