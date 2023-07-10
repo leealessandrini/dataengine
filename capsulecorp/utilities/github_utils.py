afcffe59b3dbb4808c9d6c1c41eea6991634b9c9
Lee Alessandrini <38591905+leealessandrini@users.noreply.github.com>
2023-07-10 16:49:18 -0400

    v0.1.1: Updated github utility methods.

--- capsulecorp/utilities/github_utils.py
+++ capsulecorp/utilities/github_utils.py
@@ -1,7 +1,7 @@
 from github import Github
 
 
-def _get_gh_objects(host, token, repo_name, branch, repo_location):
+def get_repo_and_contents(host, token, repo_name, branch, repo_location):
     """
         This method will get the GitHub contents object provided arguments.
 
@@ -17,30 +17,25 @@ def _get_gh_objects(host, token, repo_name, branch, repo_location):
             github ContentFile object
     """
     # Initialize GitHub instance using GHE Token
-    g = Github(base_url=host, login_or_token=token)
+    github_instance = Github(base_url=host, login_or_token=token)
     # Return repo and contents objects
-    repo = g.get_repo(repo_name)
+    repo = github_instance.get_repo(repo_name)
     return repo, repo.get_contents(repo_location, ref=branch)
 
 
-def update_source(host, token, repo_name, branch, source_code, repo_location):
+def update_source_code(repo, contents, branch, source_code):
     """
         This method will update source code in GitHub using local source code.
 
         Arguments:
-            host (str): GitHub URL
-            token (str): GitHub API token
-            repo_name (str): name of GitHub repository
+            repo (github.Repository.Repository): GitHub repository instance
+            contents (github.ContentFile.ContentFile): file contents
             branch (str): name of GitHub branch
             source_code (str): source code
-            repo_location (str): local path to source code location in repo
 
         Returns:
             commit dict
     """
-    # Get gh contents object
-    repo, contents = _get_gh_objects(
-        host, token, repo_name, branch, repo_location)
     # Update source code
     return repo.update_file(
         contents.path, "Automated code update.", source_code, contents.sha,
