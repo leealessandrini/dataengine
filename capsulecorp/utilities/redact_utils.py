43fee102abf01de23c0c07a5ca51844eb54f7db9
Lee Alessandrini <38591905+leealessandrini@users.noreply.github.com>
2023-09-11 17:52:27 -0400

    0.2.7: Added initial IP Address logic to redact_utils.

--- capsulecorp/utilities/redact_utils.py
+++ capsulecorp/utilities/redact_utils.py
@@ -3,6 +3,44 @@ import random
 
 
 MAC_REGEX = re.compile(r"((?:[0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})")
+IPv4_REGEX = re.compile(
+    r"(?<![.\w])"  # Negative lookbehind
+    r"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    r"\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    r"\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    r"\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    r"(?!\w)"  # Negative lookahead for only word characters
+)
+# Partial source: https://stackoverflow.com/questions/53497
+IPv6_REGEX = re.compile(
+    r"(?<![.\w])"  # Negative lookbehind
+    r"("  
+    r"(([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|"
+    r"(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|"
+    r"((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|"
+    r"(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|"
+    r":((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|"
+    r"(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|"
+    r"((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|"
+    r"(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|"
+    r"((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|"
+    r"(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|"
+    r"((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|"
+    r"(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|"
+    r"((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|"
+    r"(:(((:[0-9A-Fa-f]{1,4}){1,7})|"
+    r"((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)"
+    r"(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))"
+    r")"
+    r"(?!\w)"  # Negative lookahead for only word characters
+)
+
 
 
 def find_unique_macs(text, case=None):
@@ -76,3 +114,112 @@ def redact_macs_from_text(text, mac_map=None, case=None):
         redacted_text = redacted_text.replace(og_mac.lower(), redacted_mac)
 
     return redacted_text, mac_map
+
+
+def find_unique_ipv4(text):
+    """
+    Finds and returns the unique IPv4 addresses in a given text.
+    
+    Args:
+        text (str): The text to search for IPv4 addresses.
+        
+    Returns:
+        list: A sorted list of unique IPv4 addresses found in the text.
+    """
+    ipv4_addresses = re.findall(IPv4_REGEX, text)
+    unique_ipv4_addresses = list(set(ipv4_addresses))
+    unique_ipv4_addresses.sort()
+
+    return unique_ipv4_addresses
+
+
+def find_unique_ipv6(text, case=None):
+    """
+    Finds and returns the unique IPv6 addresses in a given text.
+    
+    Args:
+        text (str): The text to search for IPv6 addresses.
+        
+    Returns:
+        list: A sorted list of unique IPv6 addresses found in the text.
+    """
+    ipv6_addresses = [
+        match[0] for match in re.findall(IPv6_REGEX, text)]
+    if case == "upper":
+        ipv6_addresses = [ipv6.upper() for ipv6 in ipv6_addresses]
+    elif case == "lower":
+        ipv6_addresses = [ipv6.lower() for ipv6 in ipv6_addresses]
+    unique_ipv6_addresses = list(set(ipv6_addresses))
+    unique_ipv6_addresses.sort()
+
+    return unique_ipv6_addresses
+
+
+def generate_random_ipv4():
+    """
+    Generates a random IPv4 address.
+    
+    Returns:
+        str: A random IPv4 address.
+    """
+    return ".".join(str(random.randint(0, 255)) for _ in range(4))
+
+
+def generate_random_ipv6():
+    """
+    Generates a random IPv6 address.
+    
+    Returns:
+        str: A random IPv6 address.
+    """
+    return ":".join("{:x}".format(random.randint(0, 0xFFFF)) for _ in range(8))
+
+
+def redact_ip_addresses_from_text(text, ip_address_map=None, case=None):
+    """
+    Provided some text, redact the original ip addresses.
+
+    Args:
+        text (str): text string
+        ip_address_map (dict): key value pairs of og addresses and random ones
+        case (str): specify whether to cast addresses to uppercase or lowercase
+
+    Returns:
+        redacted text and updated ip address map
+    """
+    # Pull unique mac lists
+    ipv4_addresses = find_unique_ipv4(text)
+    ipv6_addresses = find_unique_ipv6(text, case=case)
+    # If existing map is passed update it
+    if ip_address_map:
+        # Update IPv4 Addresses
+        ip_address_map.update({
+            og_ip_address: generate_random_ipv4()
+            for og_ip_address in ipv4_addresses
+            if og_ip_address not in ip_address_map})
+        # Update IPv6 Addresses
+        ip_address_map.update({
+            og_ip_address: generate_random_ipv6()
+            for og_ip_address in ipv6_addresses
+            if og_ip_address not in ip_address_map})
+    # Otherwise create map of original mac address to random mac address
+    else:
+        ip_address_map = {
+            og_ip_address: generate_random_ipv4()
+            for og_ip_address in ipv4_addresses}
+        ip_address_map.update({
+            og_ip_address: generate_random_ipv6()
+            for og_ip_address in ipv6_addresses})
+    # Replace instances of macs in text
+    redacted_text = text
+    # Replace each original mac with a redacted mac
+    for og_ip_address, redacted_ip_address in ip_address_map.items():
+        # TODO: Handle mixed case in text
+        # Replace uppercase
+        redacted_text = redacted_text.replace(
+            og_ip_address.upper(), redacted_ip_address)
+        # Replace lowercase
+        redacted_text = redacted_text.replace(
+            og_ip_address.lower(), redacted_ip_address)
+
+    return redacted_text, ip_address_map
--- tests/utilities/test_redact_utils.py
+++ tests/utilities/test_redact_utils.py
@@ -120,3 +120,135 @@ def test_redact_macs_from_text_case_sensitivity():
     assert all(mac == mac.upper() for mac in mac_map.keys())
     assert redact_utils.find_unique_macs(text) == [
         mac_map["00:1A:2B:3C:4D:5E"]]
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('192.168.1.1', ['192.168.1.1']),
+    ('0.0.0.0', ['0.0.0.0']),
+    ('255.255.255.255', ['255.255.255.255']),
+    ('>192.168.1.1<', ["192.168.1.1"]),
+    ('The IP is 10.0.0.2.', ['10.0.0.2']),
+    ('Two IPs: 192.168.0.1, 172.16.0.2', ['192.168.0.1', '172.16.0.2']),
+])
+def test_valid_ipv4(test_input, expected):
+    assert redact_utils.IPv4_REGEX.findall(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('192.168.1.256', []),
+    ('192.168.1', []),
+    ('192.168.1.300', []),
+    ('.192.168.1.1', []),
+    ('a192.168.1.1', []),
+])
+def test_invalid_ipv4(test_input, expected):
+    assert redact_utils.IPv4_REGEX.findall(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('192.168.1.1', ['192.168.1.1']),
+    ('0.0.0.0', ['0.0.0.0']),
+    ('255.255.255.255', ['255.255.255.255']),
+    ('The IP is 10.0.0.2.', ['10.0.0.2']),
+    ('Two IPs: 192.168.0.1, 172.16.0.2', ['172.16.0.2', '192.168.0.1']),
+    ('No IPs here!', []),
+    ('.192.168.1.1', []),
+    ('192.168.1.300', []),
+    ('', []),
+])
+def test_find_unique_ipv4(test_input, expected):
+    assert redact_utils.find_unique_ipv4(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('2001:0db8:85a3:0000:0000:8a2e:0370:7334', ['2001:0db8:85a3:0000:0000:8a2e:0370:7334']),
+    ('::1', ['::1']),
+    ('::', ['::']),
+    ('The IPv6 is 2001:0db8:85a3:0000:0000:8a2e:0370:7334.', ['2001:0db8:85a3:0000:0000:8a2e:0370:7334']),
+    ('Two IPv6: 2001:0db8:85a3:0000:0000:8a2e:0370:7334, fe80::202:b3ff:fe1e:8329', 
+     ['2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'fe80::202:b3ff:fe1e:8329']),
+    ('No IPs here!', []),
+    ('.2001:0db8:85a3:0000:0000:8a2e:0370:7334', []),
+    ('2001:0db8:85a3:0000:0000:8a2e:0370:xyz', []),
+    ('', []),
+])
+def test_ipv6_regex(test_input, expected):
+    ipv6_addresses = [
+        match[0] for match in redact_utils.IPv6_REGEX.findall(test_input)]
+    assert ipv6_addresses == expected
+
+
+@pytest.mark.parametrize("test_input,case,expected", [
+    (
+        'Two IPv6: 2001:0db8:85a3:0000:0000:8a2e:0370:7334, ::1', None,
+        ['2001:0db8:85a3:0000:0000:8a2e:0370:7334', '::1']),
+    ('Another IPv6: ::', None, ['::']),
+    (
+        'IPv6 with different cases: 2001:0db8::ff00:42:8329 and 2001:0DB8::FF00:42:8329',
+        'lower', ['2001:0db8::ff00:42:8329']),
+    (
+        'IPv6 with different cases: 2001:0db8::ff00:42:8329 and 2001:0DB8::FF00:42:8329',
+        'upper', ['2001:0DB8::FF00:42:8329']),
+    ('No IPv6 here!', None, [])
+])
+def test_find_unique_ipv6_parametrized(test_input, case, expected):
+    result = redact_utils.find_unique_ipv6(test_input, case=case)
+    assert result == expected, f"For {test_input}, expected {expected} but got {result}"
+
+
+def test_generate_random_ipv4_type():
+    ipv4 = redact_utils.generate_random_ipv4()
+    assert isinstance(ipv4, str)
+
+
+def test_generate_random_ipv4_format():
+    ipv4 = redact_utils.generate_random_ipv4()
+    assert bool(redact_utils.IPv4_REGEX.match(ipv4))
+
+
+def test_generate_random_ipv4_uniqueness():
+    ipv4_addresses = {redact_utils.generate_random_ipv4() for _ in range(100)}
+    assert len(ipv4_addresses) == 100
+
+
+def test_generate_random_ipv6_type():
+    ipv6 = redact_utils.generate_random_ipv6()
+    assert isinstance(ipv6, str)
+
+
+def test_generate_random_ipv6_format():
+    ipv6 = redact_utils.generate_random_ipv6()
+    assert bool(redact_utils.IPv6_REGEX.match(ipv6))
+
+
+def test_generate_random_ipv6_uniqueness():
+    ipv6_addresses = {redact_utils.generate_random_ipv6() for _ in range(100)}
+    assert len(ipv6_addresses) == 100
+
+
+@pytest.mark.parametrize("input_text, expected_map, case", [
+    (
+        "My IPs are 192.168.1.1 and 10.0.0.2.",
+        {"192.168.1.1": None, "10.0.0.2": None},
+        None
+    ),
+    (
+        "IPv6: 2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+        {"2001:0db8:85a3:0000:0000:8a2e:0370:7334": None},
+        None
+    ),
+    (
+        "IPv6: 2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+        {"2001:0DB8:85A3:0000:0000:8A2E:0370:7334": None},
+        "upper"
+    ),
+])
+def test_redact_ip_addresses_from_text(input_text, expected_map, case):
+    redacted_text, ip_address_map = redact_utils.redact_ip_addresses_from_text(
+        input_text, case=case)
+    # Check if all expected IP addresses are in the map and have been replaced
+    for og_ip_address in expected_map.keys():
+        assert og_ip_address in ip_address_map, f"{og_ip_address} not in {ip_address_map}"
+        redacted_ip_address = ip_address_map[og_ip_address]
+        assert og_ip_address not in redacted_text
+        assert redacted_ip_address not in input_text, f"{redacted_ip_address} found in original text"
+        assert redacted_ip_address in redacted_text, f"{redacted_ip_address} not found in redacted text"
