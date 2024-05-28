03f0eeaf30e121009b11be80caedfca3b6e636e2
Lee Alessandrini <38591905+leealessandrini@users.noreply.github.com>
2024-05-28 08:22:41 -0400

    v0.0.57: Updated postgres query for checking table existence (there was a syntax error).

--- dataengine/assets.py
+++ dataengine/assets.py
@@ -376,7 +376,8 @@ class Database(Asset):
             # PostgreSQL query
             query = f"""
             SELECT EXISTS (
-                SELECT FROM information_schema.tables 
+                SELECT 1
+                FROM information_schema.tables 
                 WHERE
                     table_schema = '{schema_name}' AND
                     table_name = '{table_name}'
