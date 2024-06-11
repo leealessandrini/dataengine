3c2e91a980fdbc2b6743db26ae23e79fd90ea441
Lee Alessandrini <38591905+leealessandrini@users.noreply.github.com>
2024-06-11 14:08:03 -0700

    v0.0.67: Added functions to glue utilities for instantiating a glue client and checking the status of a glue crawler.

--- dataengine/utilities/glue_utils.py
+++ dataengine/utilities/glue_utils.py
@@ -2,6 +2,35 @@
 AWS Glue Utility Functions
 """
 import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+import time
+
+
+def instantiate_glue_client(
+        aws_access_key_id=None, aws_secret_access_key=None, aws_region_name=None):
+    """
+    Instantiate an AWS Glue client with optional AWS credentials and region.
+
+    Args:
+        aws_access_key_id (str, optional): AWS access key ID. Defaults to None.
+        aws_secret_access_key (str, optional): AWS secret access key. Defaults to None.
+        aws_region_name (str, optional): AWS region name. Defaults to None.
+
+    Returns:
+        boto3.client: A Boto3 Glue client.
+    """
+    # Setup session arguments if applicable
+    session_args = {}
+    if aws_access_key_id and aws_secret_access_key:
+        session_args['aws_access_key_id'] = aws_access_key_id
+        session_args['aws_secret_access_key'] = aws_secret_access_key
+    if aws_region_name:
+        session_args['region_name'] = aws_region_name
+    # Instantiate glue client
+    session = boto3.Session(**session_args)
+    glue_client = session.client('glue')
+
+    return glue_client
 
 
 def create_glue_table(
@@ -169,4 +198,62 @@ def start_glue_crawler(
         return True
     except Exception as e:
         print(f"Failed to start crawler '{crawler_name}': {str(e)}")
+        return False
+
+
+def check_crawler_status(
+        crawler_name, aws_access_key_id=None, aws_secret_access_key=None,
+        aws_region_name=None, sleep_time=30):
+    """
+    Check the status of an AWS Glue crawler.
+
+    Args:
+        crawler_name (str): The name of the Glue crawler.
+        aws_access_key_id (str, optional): AWS access key ID. Defaults to None.
+        aws_secret_access_key (str, optional): AWS secret access key. Defaults to None.
+        aws_region_name (str, optional): AWS region name. Defaults to None.
+        sleep_time (int, optional): Time to sleep between status checks in seconds. Defaults to 30.
+
+    Returns:
+        bool: True if the crawler ran successfully, False otherwise.
+    
+    Raises:
+        NoCredentialsError: If AWS credentials are not available.
+        PartialCredentialsError: If incomplete AWS credentials are provided.
+        Exception: If any other error occurs.
+    """
+    # Instantiate glue client
+    glue_client = instantiate_glue_client(
+        aws_access_key_id, aws_secret_access_key, aws_region_name)
+    # Attempt to get the crawler's status
+    try:
+        while True:
+            response = glue_client.get_crawler(Name=crawler_name)
+            crawler_state = response['Crawler']['State']
+            last_crawl_status = response['Crawler']['LastCrawl'].get('Status', 'UNKNOWN')
+            print(f"Crawler {crawler_name} is currently {crawler_state}. Last crawl status: {last_crawl_status}")
+            if crawler_state in ['READY', 'STOPPED']:
+                if last_crawl_status == 'SUCCEEDED':
+                    print(f"Crawler {crawler_name} has completed successfully.")
+                    return True
+                else:
+                    print(f"Crawler {crawler_name} has completed with status: {last_crawl_status}.")
+                    return False
+            elif crawler_state == 'STOPPING' or crawler_state == 'RUNNING':
+                print(f"Crawler {crawler_name} is still running. Checking again in {sleep_time} seconds.")
+                time.sleep(sleep_time)
+            else:
+                print(f"Crawler {crawler_name} is in an unknown state: {crawler_state}")
+                return False
+    except glue_client.exceptions.EntityNotFoundException:
+        print(f"Crawler {crawler_name} not found.")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available.")
+        return False
+    except PartialCredentialsError:
+        print("Incomplete credentials provided.")
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return False
