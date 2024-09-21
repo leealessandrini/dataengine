099926b5c353713f0402e0efdda59a8645da8cda
Lee <alessandrinil@gmail.com>
2024-09-21 18:45:13 -0400

    v0.0.84: Enforced marshmallow versions to be >=3.13.0 do to default values not being loaded properly.

--- dataengine/version.py
+++ dataengine/version.py
@@ -2,5 +2,5 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version_info__ = (0, 0, 83)
+__version_info__ = (0, 0, 84)
 __version__ = '.'.join(map(str, __version_info__))
